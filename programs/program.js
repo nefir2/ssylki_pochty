4e616ed08070e47a771f1ce144961dba640cd8cf
nefir2 <vadim.ahtamov@gmail.com>
2022-02-28 00:07:45 +0300

    touchable text, readable code
    исправлено нажатие на текст у галочки и самой галочки;
    поправлена читаемость кода

--- programs/program.js
+++ programs/program.js
@@ -1,25 +1,21 @@
 //переменные
 let olay, inpt, text;
-var yes;
+var bool;
 //места из html
 inpt = document.getElementById('btn'); //галочка (checkbox)
-text = document.getElementById('mini');
 olay = document.getElementsByClassName('targ'); //все теги "a"
-//нажатия
+//нажатие на галочку
 inpt.addEventListener('click', trueno);
-text.addEventListener('click', trueno);
 //значение для переменной при открытии страницы
-yes = false;
+bool = false;
 //функция при нажатии на галочку
 function trueno() {
 	//решение добавлять атрибут или нет
-	yes = !yes; //при срабатывании функции меняется значение переменной на противоположное
-	if (yes) {
+	bool = !bool; //при срабатывании функции меняется значение переменной на противоположное
+	if (bool) {
 		for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank');
-		inpt.setAttribute('checked', '');
 	}
 	else {
 		for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target');
-		inpt.removeAttribute('checked');
 	}
 }
