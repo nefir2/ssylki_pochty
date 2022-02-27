8354f008af6464aea3da295416cdfd08e6e61d99
nefir2 <vadim.ahtamov@gmail.com>
2022-02-27 20:34:35 +0300

    added button text
    при нажатии на текст около галочки срабатывает функция, что работала только при нажатии на галочку

--- programs/program.js
+++ programs/program.js
@@ -1,11 +1,13 @@
 //переменные
-let olay, inpt;
+let olay, inpt, text;
 var yes;
 //места из html
 inpt = document.getElementById('btn'); //галочка (checkbox)
+text = document.getElementById('mini');
 olay = document.getElementsByClassName('targ'); //все теги "a"
-//нажатие на галочку
+//нажатия
 inpt.addEventListener('click', trueno);
+text.addEventListener('click', trueno);
 //значение для переменной при открытии страницы
 yes = false;
 //функция при нажатии на галочку
@@ -13,11 +15,11 @@ function trueno() {
 	//решение добавлять атрибут или нет
 	yes = !yes; //при срабатывании функции меняется значение переменной на противоположное
 	if (yes) {
-		for (var i = 0; i < olay.length; i++)
-			olay[i].setAttribute('target', '_blank'); 
-		}
-	else { 
-		for (var i = 0; i < olay.length; i++)
-			olay[i].removeAttribute('target'); 
-		}
+		for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank');
+		inpt.setAttribute('checked', '');
+	}
+	else {
+		for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target');
+		inpt.removeAttribute('checked');
+	}
 }
