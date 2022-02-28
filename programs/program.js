ee67283bc79b106bcd78af1cede3960faf900a63
nefir2 <vadim.ahtamov@gmail.com>
2022-02-28 13:31:21 +0300

    code formatting
    размер фоновой картинки изменён со значений на выравнивание по левому краю;
    сжата функция труено, добавлена информация в комментарий

--- programs/program.js
+++ programs/program.js
@@ -3,7 +3,7 @@ let olay, inpt, text;
 var bool;
 //места из html
 inpt = document.getElementById('btn'); //галочка (checkbox)
-olay = document.getElementsByClassName('targ'); //все теги "a"
+olay = document.getElementsByClassName('targ'); //все теги "a" с классом "targ"
 //нажатие на галочку
 inpt.addEventListener('click', trueno);
 //значение для переменной при открытии страницы
@@ -12,10 +12,6 @@ bool = false;
 function trueno() {
 	//решение добавлять атрибут или нет
 	bool = !bool; //при срабатывании функции меняется значение переменной на противоположное
-	if (bool) {
-		for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank');
-	}
-	else {
-		for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target');
-	}
+	if (bool) { for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank'); }
+	else { for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target'); }
 }
