af04972612e5fddaa029dad0dc73555c3f7b883c
nefir2 <vadim.ahtamov@gmail.com>
2022-04-30 00:04:45 +0300

    nt

--- programs/program.js
+++ programs/program.js
@@ -1,17 +1,20 @@
 //переменные
 let olay, inpt, text;
-var bool;
+//инициализация переменной при открытии страницы
+var bool = false;
 //места из html
 inpt = document.getElementById('btn'); //галочка (checkbox)
 olay = document.getElementsByClassName('targ'); //все теги "a" с классом "targ"
 //нажатие на галочку
 inpt.addEventListener('click', trueno);
-//значение для переменной при открытии страницы
-bool = false;
 //функция при нажатии на галочку
 function trueno() {
 	//решение добавлять атрибут или нет
 	bool = !bool; //при срабатывании функции меняется значение переменной на противоположное
-	if (bool) { for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank'); }
-	else { for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target'); }
+	if (bool) { 
+		for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank'); 
+	}
+	else { 
+		for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target'); 
+	}
 }
