8d08f7315d6abd97c4fe95cf9f7f9dcfc6b8e431
nefir2 <vadim.ahtamov@gmail.com>
2022-05-01 18:55:05 +0300

    now it can remember your setting

--- programs/program.js
+++ programs/program.js
@@ -1,20 +1,28 @@
 //переменные
 let olay, inpt, text;
-//инициализация переменной при открытии страницы
-var bool = false;
+let bool = false;
+const locName = "isChecked";
 //места из html
 inpt = document.getElementById('btn'); //галочка (checkbox)
 olay = document.getElementsByClassName('targ'); //все теги "a" с классом "targ"
+//значение для переменной при открытии страницы
+if (localStorage.hasOwnProperty(locName)) {
+	if (localStorage.getItem(locName) == "true") bool = true;
+	else bool = false;
+}
+//синхронизация значений с памятью
+inpt.checked = bool;
+setAtrb(bool);
 //нажатие на галочку
 inpt.addEventListener('click', trueno);
 //функция при нажатии на галочку
 function trueno() {
 	//решение добавлять атрибут или нет
 	bool = !bool; //при срабатывании функции меняется значение переменной на противоположное
-	if (bool) { 
-		for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank'); 
-	}
-	else { 
-		for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target'); 
-	}
+	setAtrb(bool);
+	localStorage.setItem(locName, bool);
+}
+function setAtrb(set){
+	if (set) { for (var i = 0; i < olay.length; i++) olay[i].setAttribute('target', '_blank'); }
+	else { for (var i = 0; i < olay.length; i++) olay[i].removeAttribute('target'); }
 }
