ff34e2c243204d3504c7b8cfc2fc95b0f5c7a956
nefir2 <vadim.ahtamov@gmail.com>
2022-05-09 23:07:15 +0300

    now you can off background

--- programs/program.js
+++ programs/program.js
@@ -1,25 +1,43 @@
 //переменные
-let olay, inpt, text;
-let bool = true; //стоковое значение для переменной, при первом открытии сайта
+let olay, blank, switchbg, text;
+let bool = true, boolbg = false; //стоковое значение для переменной, при первом открытии сайта
 const locName = "isChecked";
+const sbg = "Background";
 //места из html
-inpt = document.getElementById('btn'); //галочка (checkbox)
+blank = document.getElementById('tab'); //галочка для открытия нового окна (checkbox)
+switchbg = document.getElementById("bg"); //галочка для выключателя фона
 olay = document.getElementsByClassName('targ'); //все теги "a" с классом "targ"
 //значение для переменной при открытии страницы
 if (localStorage.hasOwnProperty(locName)) {//получение значения переменной, если уже есть переменная в памяти
 	if (localStorage.getItem(locName) == "true") bool = true; //дополнительные сравнения, так как в память запоминается строкой
 	else bool = false; 
 } //иначе стоковое значение
+if (localStorage.hasOwnProperty(sbg)){
+	if (localStorage.getItem(sbg) == "true") boolbg = true;
+	else boolbg = false;
+}
 //синхронизация значений с памятью
-inpt.checked = bool; //выставление галочки
+blank.checked = bool; //выставление галочки
 toggleAtrb(bool); //выставление атрибутов тегам "a", какие указаны в памяти
+switchbg.checked = boolbg;
+toggleBg(boolbg);
 //нажатие на галочку
-inpt.addEventListener('click', trueno);
+blank.addEventListener('click', trueno);
+switchbg.addEventListener('click', changeBg)
 //функция при нажатии на галочку
 function trueno() {
 	bool = !bool; //при срабатывании функции меняется значение переменной на противоположное
 	toggleAtrb(bool); //выставление атрибутов тегам "a"
 	localStorage.setItem(locName, bool); //сохранение значения в память
+}
+function changeBg() {
+	boolbg = !boolbg;
+	toggleBg(boolbg);
+	localStorage.setItem(sbg, boolbg);
+}
+function toggleBg(what){
+	if (what) document.body.style.backgroundImage = 'url(../pics/phon/girl_and_the_pond.jpg)';
+	else document.body.style.backgroundImage = '';
 }
 function toggleAtrb(set) 
 {//решение добавлять атрибут или нет
